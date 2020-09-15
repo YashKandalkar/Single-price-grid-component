d9fd95eaea20957857d2617720cfc7ccc7400c35
YashKandalkar <yashsk.yk@gmail.com>
2020-09-16 00:17:10 +0530

    Updates

---
+++ precache-manifest.56feba2b160266be8a9b62b611685916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2f5feccfd04157932db4e078754dc4",
+    "url": "/Single-price-grid-component/index.html"
+  },
+  {
+    "revision": "ea698b9eef93ce940c99",
+    "url": "/Single-price-grid-component/static/css/main.170aaf9e.chunk.css"
+  },
+  {
+    "revision": "a3213d0f9c2c4fc744e6",
+    "url": "/Single-price-grid-component/static/js/2.e7026cf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Single-price-grid-component/static/js/2.e7026cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea698b9eef93ce940c99",
+    "url": "/Single-price-grid-component/static/js/main.dfa10dd8.chunk.js"
+  },
+  {
+    "revision": "bdd27300e83da750da40",
+    "url": "/Single-price-grid-component/static/js/runtime-main.cbd57f89.js"
+  }
+]);
